5e2757dfd5b682a69cd616ffc801d3af856c4a7d
Hermitxxx <cvenom477@gmail.com>
2026-05-18 13:11:44 +0600

    made two get apis for all docs and top docs

---
+++ index.js
@@ -0,0 +1,58 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+const port = 5000
+dotenv.config()
+
+const uri = process.env.MONGO_URI
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db('clinovo')
+        const doctorsColl = db.collection('doctors')
+
+        // get all doctors
+        app.get('/doctors', async (req, res) => {
+            const cursor = doctorsColl.find()
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+        // get top doctors
+        app.get('/top-doctors', async (req, res) => {
+            const query = {
+                rating: -1
+            }
+            const result = await doctorsColl.find().sort(query).limit(3).toArray()
+            res.json(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('server running on port', port);
+})
